f59b6c75a901c8a7ed6d8033c99bf66ded2b8c85
mac <mac@DESKTOP-T73Q40O>
2019-11-02 20:47:57 -0400

    my new commits

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.Write("Hello World !");
-            Console.Write("Mike!!!!");
+            Console.Write("myNew Changes");
            
         }
     }
