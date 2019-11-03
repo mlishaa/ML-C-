78001b5c8a6c6ba4e28b9cfeb769dc443156e494
mlishaa <57273606+mlishaa@users.noreply.github.com>
2019-11-02 20:51:10 -0400

    commits from the internet

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.Write("Hello World !");
-            Console.Write("myNew Changes");
+            Console.Write("some thing to pull");
            
         }
     }
